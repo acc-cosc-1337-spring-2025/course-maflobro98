e510c9e41c339fb89bf2248c94d81a102168b0b3
maflobro98 <marco.flores@g.austincc.edu>
2025-03-09 05:39:11 +0000

    func assigement

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,62 @@
 //add include statements
-
+#include <iostream>
+using namespace std;
 //add function code here
+
+double get_gc_content(const string& dna)
+{
+    double counter = 0.0;
+    int dnaSize = dna.length(); 
+    
+    for(int i = 0; i < dnaSize; i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C')
+        {
+            counter++;
+        }
+    }
+
+    return counter/dnaSize;
+}   
+
+string get_reverse_string(string dna)
+{
+    string newDNA = "";
+    int dnaSize = dna.length();
+    for(int i = dnaSize - 1; i >= 0; i--)
+    {
+        newDNA = newDNA + dna[i];
+    }
+    return newDNA;
+}
+
+string get_dna_complement(string dna)
+{
+    string reverseDNA = get_reverse_string(dna);
+    string complimentDNA = "";
+    int dnaSize = reverseDNA.length();
+    
+
+    for(int i = 0; i < dnaSize; i++)
+    {
+        if(reverseDNA[i] == 'A')
+        {
+            complimentDNA = complimentDNA + "T";
+        }
+        else if(reverseDNA[i] == 'T')
+        {
+            complimentDNA = complimentDNA + "A";
+        }
+        else if(reverseDNA[i] == 'C')
+        {
+            complimentDNA = complimentDNA + "G";
+        }
+        else if(reverseDNA[i] == 'G')
+        {
+            complimentDNA = complimentDNA + "C";
+        }
+    }
+    return complimentDNA;
+}
+
+
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
+#include "func.h"
+using namespace std;
+
 
 int main() 
 {
+	int userIn;
+	string userDNA = "";
+	cout << "Menu\n1- Get GC Content\n2- Get DNA Compliment\n3- Exit" << endl;
+	cin >> userIn;
+
+	if(userIn == 1)
+	{
+		cout << "\nEnter DNA string: ";
+		cin >> userDNA;
+		cout << endl;
+		cout << get_gc_content(userDNA) << endl;
+	}
+	else if(userIn == 2)
+	{
+		cout << "\nEnter DNA string: ";
+		cin >> userDNA;
+		cout << endl;
+		cout << get_dna_complement(userDNA) << endl;
+	}
+	else if(userIn == 3)
+	{
+		cout << "Good Bye!";
+	}
 
 	return 0;
 }
